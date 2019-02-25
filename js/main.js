78ad770e6195c493a877e85f63476f62e14db219
somerom <33866801+somerom@users.noreply.github.com>
2019-02-25 13:03:45 +0200

    alku

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const nappi = document.getElementById("nappi");
+nappi.addEventListener("click", find);
+
+function  find() {
+    //hakee rajapinnasta lähdöllä ja saapumisoaikalla reitin
+    //laskee taksien hinnat, tulostaa ne sivulle numeron kera.
+}
