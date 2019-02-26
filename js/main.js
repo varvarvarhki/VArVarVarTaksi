253c19f3574096baa607edbaad668103b2cf0da7
somerom <33866801+somerom@users.noreply.github.com>
2019-02-26 14:05:00 +0200

    26.2.searchia

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 const nappi = document.getElementById("nappi");
-const address1 = document.getElementById("lahto");
-const address2 = document.getElementById("saapuminen");
-nappi.addEventListener("click", changeAddress);
+let address1 = document.getElementById("lahto");
+let address2 = document.getElementById("saapuminen");
+nappi.addEventListener("click", search);
 
-function  changeAddress() {
-
+function  changeAddress( add) {
+    let cordinate;
+    // tähän reverse geocoding
+    return cordinate;
+}
+function search() {
+    let searchaddress = "https://api.openrouteservice.org/directions?api_key=5b3ce3597851110001cf62488bdc9c76f18d4844942745fee4a44696&coordinates="
+    let searchaddress2 = "&profile:driving-car&format:geojson&instructions:false"
+    let coordinates = chnageAddress(address1) + "|" +changeAddress(address2);
+    searchaddress += coordinates;
 }
