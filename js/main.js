076747e0c362e1440aa6729d09dee0af0cdd691a
somerom <33866801+somerom@users.noreply.github.com>
2019-03-04 11:48:21 +0200

    4.3.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const nappi = document.getElementById("nappi")
 nappi.addEventListener("click", changeAddress);
 let coordinates = [];
+let linestring = [];
 
 function  changeAddress() {
     let searchadd = "https://api.openrouteservice.org/geocode/search?api_key=5b3ce3597851110001cf62488bdc9c76f18d4844942745fee4a44696&text=";
@@ -17,8 +18,21 @@ function  changeAddress() {
         return vastaus.json();
 
     }).then(function(json) {
-        coordinates[0]="" + json['bbox'][0] + "," +json['bbox'][1];
-        console.log((coordinates[0]));
+        if(json['features'].length===0) {
+            if(coordinates[0]!==undefined) {
+                coordinates[0].delete();
+            }
+            linestring  = []
+            document.getElementById("saapuminen").value = null;
+            document.getElementById("lahto").value = null;
+            location.reload();
+            confirm("Paikkaa ei löydy Pääkaupunkiseudulta, koeile uudestaan.");
+
+        } else {
+            coordinates[0] = "" + json['bbox'][0] + "," + json['bbox'][1];
+            console.log(coordinates[0]);
+        }
+
     }).catch(function(error) {
 
         console.log(error);
@@ -29,8 +43,21 @@ function  changeAddress() {
         return vastaus.json();
 
     }).then(function(json) {
-        coordinates[1]= "" + json['bbox'][0] + "," +json['bbox'][1];
-        console.log(coordinates[1]);
+        console.log(json);
+        console.log(json['features'].length===0);
+        if(json['features'].length===0)  {
+            if(coordinates[1]!==undefined) {
+                coordinates[1].delete();
+            }
+            linestring =[];
+            document.getElementById("saapuminen").value = null;
+            document.getElementById("lahto").value = null;
+            location.reload();
+            confirm("Paikkaa ei löydy Pääkaupunkiseudulta, koeile uudestaan.");
+        } else {
+            coordinates[1] = "" + json['bbox'][0] + "," + json['bbox'][1];
+            console.log(coordinates[1]);
+        }
     }).catch(function(error) {
 
         console.log(error);
@@ -43,8 +70,9 @@ function  changeAddress() {
 }
 
 function search() {
-    if (coordinates = 24.99935, 20.20787 || 25.012523, 60.09489 || 24.65104, 60.20956 || 24.716896, 60.135897)
+    if (coordinates.includes("24.99935,20.20787" || "25.012523, 60.09489" || "24.65104, 60.20956" || "24.716896, 60.135897")) {
         confirm("Paikkaa ei ole");
+    }
     else {
     let searchaddress =  "https://api.openrouteservice.org/directions?api_key=5b3ce3597851110001cf62488bdc9c76f18d4844942745fee4a44696&coordinates=";
     let searchaddress2 = "&profile=driving-car&format=geojson";
@@ -68,6 +96,7 @@ function search() {
     }).then(function(geojson) {
         if(stat!==404) {
             useData(geojson);
+
         }
     }).catch(function(error) {
         console.log(error);
@@ -76,23 +105,139 @@ function search() {
 function useData(geojson) {
     console.log(geojson);
     let i = coordinates[0].indexOf(",");
-    let cord1 = coordinates[0].substring(0,i);
-    let cord2 = coordinates[0].substring(i +1, coordinates[0].length);
-    let c1= parseFloat(cord1);
+    let cord1 = coordinates[0].substring(0, i);
+    let cord2 = coordinates[0].substring(i + 1, coordinates[0].length);
+    let j = coordinates[0].indexOf(",");
+    let cord3 = coordinates[1].substring(0, i);
+    let cord4 = coordinates[1].substring(i + 1, coordinates[1].length);
+    let c1 = parseFloat(cord1);
     let c2 = parseFloat(cord2);
+    let c3 = parseFloat(cord3);
+    let c4 = parseFloat(cord4);
 
     mapboxgl.accessToken = 'pk.eyJ1IjoidmFydmFydmFyaGtpIiwiYSI6ImNqc29meHo3aDBrY2EzeWp2NnNsdTNlejIifQ.Lr5nSKYjznoAIrtfgg6jpQ';
     let map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/mapbox/streets-v9',
-        center: [c1,c2],
-        zoom: 
+        center: [c1, c2],
+        zoom: 15
     });
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
-        id: 'mapbox.streets',
-        accessToken: 'pk.eyJ1IjoidmFydmFydmFyaGtpIiwiYSI6ImNqc29meHo3aDBrY2EzeWp2NnNsdTNlejIifQ.Lr5nSKYjznoAIrtfgg6jpQ'
-    }).addTo(map);
+
+    for (let i = 0; i <
+    geojson['features'][0]['geometry']['coordinates'].length; i++) {
+        linestring[i] = [
+            geojson['features'][0]['geometry']['coordinates'][i][0],
+            geojson['features'][0]['geometry']['coordinates'][i][1]];
+
+    }
+    calculateDistance();
+    calculateTime();
+    map.on("load", function() {
+        /* Image: An image is loaded and added to the map. */
+        map.loadImage("https://i.imgur.com/MK4NUzI.png",
+            function(error, image) {
+                if (error) throw error;
+                map.addImage("custom-marker", image);
+                /* Style layer: A style layer ties together the source and image and specifies how they are displayed on the map. */
+                map.addLayer({
+                    id: "marker1",
+                    type: "symbol",
+                    /* Source: A data source specifies the geographic coordinate where the image marker gets placed. */
+                    source: {
+                        type: "geojson",
+                        data: {
+                            type: 'FeatureCollection',
+                            features: [
+                                {
+                                    type: 'Feature',
+                                    properties: {},
+                                    geometry: {
+                                        type: "Point",
+                                        coordinates: [c1, c2]
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    layout: {
+                        "icon-image": "custom-marker",
+                    }
+                });
+            });
+        map.loadImage("https://i.imgur.com/MK4NUzI.png",
+            function(error, image) {
+                if (error) throw error;
+                map.addImage("custom-image", image);
+                /* Style layer: A style layer ties together the source and image and specifies how they are displayed on the map. */
+                map.addLayer({
+                    id: "marker2",
+                    type: "symbol",
+                    /* Source: A data source specifies the geographic coordinate where the image marker gets placed. */
+                    source: {
+                        type: "geojson",
+                        data: {
+                            type: 'FeatureCollection',
+                            features: [
+                                {
+                                    type: 'Feature',
+                                    properties: {},
+                                    geometry: {
+                                        type: "Point",
+                                        coordinates: [c3, c4]
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    layout: {
+                        "icon-image": "custom-marker",
+                    }
+                });
+
+            });
+        map.addLayer({
+            "id": "route",
+            "type": "line",
+            "source": {
+                "type": "geojson",
+                "data": {
+                    "type": "Feature",
+                    "properties": {},
+                    "geometry": {
+                        "type": "LineString",
+                        "coordinates":
+                        linestring
+                    }
+                }
+            },
+            "layout": {
+                "line-join": "round",
+                "line-cap": "round"
+            },
+            "paint": {
+                "line-color": "#f00",
+                "line-width": 5
+            }
+        });
+
+    });
+    }
+
 }
+function calculateDistance() {
+    let sadd = "https://api.openrouteservice.org/matrix?api_key=5b3ce3597851110001cf62488bdc9c76f18d4844942745fee4a44696&profile=driving-car&locations=9.970093,48.477473%7C9.207916,49.153868%7C37.573242,55.801281%7C115.663757,38.106467&metrics=duration"""
+}
+function calculateTime() {
+    let sadd = "https://api.openrouteservice.org/matrix?api_key=5b3ce3597851110001cf62488bdc9c76f18d4844942745fee4a44696&profile=driving-car&locations=" + linestring[0];
+    let sadd2 = "&metrics=duration";
+    let cords;
+    for (let i = 1; i < linestring.length; i++) {
+        sadd += "%7C" + linestring[i];
+    }
+    sadd += sadd2;
+    fetch(sadd).then(function(answer) {
+        return answer.json();
+    }).then(function (json) {
+        //
+    }
 }
